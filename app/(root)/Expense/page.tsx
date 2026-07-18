b1fb819c8d4a32c87d6b75a56a113763421e37de
sadek1amine <148018682+sadek1amine@users.noreply.github.com>
2026-07-17 19:42:54 -0600

    Update page.tsx

--- app/(root)/Expense/page.tsx
+++ app/(root)/Expense/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 
+
 import HeaderBox from "@/components/HeaderBox";
 import RecentExpenses from "@/components/RecentExpenses";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
